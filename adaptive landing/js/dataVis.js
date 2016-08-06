1112185bfee9d8b844c1159ef57ebc7d49ec48b2
Dmitry Dolgov <domain@dot.com>
2016-08-06 13:41:21 +0300

    adaptive: video finished

--- adaptive landing/js/dataVis.js
+++ adaptive landing/js/dataVis.js
@@ -1,27 +1,22 @@
 /**
  * Created by Dmitry on 03.08.16.
  */
-function setBarWidth(dataElement, barElement, cssProperty, barPercent) {
+function setBarWidth(dataElement, barElement, barPercent) {
     var listData = [];
     $(dataElement).each(function() {
         listData.push($(this).html());
     });
-    var listMax = Math.max.apply(Math, listData);
+    //var listMax = Math.max.apply(Math, listData);
     listMax=100;
+    var width=0;
     $(barElement).each(function(index) {
-        $(this).css(cssProperty, (listData[index] / listMax) * barPercent + "%");
+        width=(listData[index] / listMax) * barPercent + "%";
+        $(this).css({
+            "animation": "grow-width 3s",
+            "animation-play-state": "running",
+            "width":width
+        });
+
     });
+    $(".dataVis1 li span.numbers").css({"display":"block"});
 }
-
-function beginVis(elemendId,dataElement,barElement, cssProperty, barPercent) {
-    var elem = document.getElementById(elementId);
-
-    var sourceBottom = elem.getBoundingClientRect().bottom + window.pageYOffset;
-
-    window.onscroll = function() {
-        if (window.pageYOffset > sourceBottom) {
-            setBarWidth(dataElement,barElement, cssProperty, barPercent);
-        }
-    };
-}
-
